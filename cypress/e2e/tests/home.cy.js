445548622824a82b22fa2b82c6f571e6f55bf59a
“dannjose” <Danilo.Santos@wolterskluwer.com>
2023-05-24 20:01:25 +0100

    docs: Adding missing methods

--- cypress/e2e/tests/home.cy.js
+++ cypress/e2e/tests/home.cy.js
@@ -1,19 +1,28 @@
 import homePage from "/pages/homePage"
 import passengercarsPage from "/pages/passengercarsPage";
+//import 'cypress-xpath';
 
 
 describe('Task 2: Automation Testingt', function() { 
   it('Validate A Class models price are between £15,000 and £60,000', function() {
-       cy.visit('https://www.mercedes-benz.co.uk');
+    Cypress.on('uncaught:exception', (err, runnable) => {
+      // returning false here prevents Cypress from
+      // failing the test
+      return false
+  })
+      homePage.visit()
 
        //Clicking on Widget Menu Item
-      homePage.clickAcceptCookies();
+      homePage.clickOnAgreeToAll();
       homePage.clickOnHatchback();
+      homePage.hoverOurModels();
       homePage.clickAclassModel();
       homePage.clickBuildYourCar();
+      homePage.selectDieselOption();
+      homePage.getPrices();
 
       //Takes a screnshots of the results
-      passengercarsPage.clickDieselOption().screenshot();
+      //passengercarsPage.clickAclassModel().screenshot();
        })
       
       })
--- pages/homePage.js
+++ pages/homePage.js
@@ -1,33 +1,77 @@
+const { click } = require("wd/lib/commands")
 
 class homePage{
     elements ={
-
+        //cookieNotice : () => cy.get('.cookie-notice'),
+        agreeLink : () => cy.contains('agree'),
         hatchBackLink : () => cy.get('#hatchback-portaledId'),
-        acceptCookies : () => cy.contains('Agree to all'),
         models: () => cy.get('dh-io-vmos_9BIGf'),
         aClassLink : () => cy.get('.wb-grid-row dh-io-vmos_3Z8XX'),
         buildYourCarLink : () => cy.get('.wb-grid-row dh-io-vmos_3Z8XX')
 
     }
 
-    clickAcceptCookies(){
-        this.elements.acceptCookies().click()
+    constructor() {
+        this.url = "https://www.mercedes-benz.co.uk"
+      }
+     
+    visit(){
+        cy.visit(this.url)
+    }
+    
+      clickOnAgreeToAll() {
+        // Wait for the button to appear
+        cy.wait(5000);
+
+        // Wait for the cookie consent banner to be visible
+        cy.contains('agree').click({ force: true });
+    
+    }
+
+    hoverOurModels(){
+        // Hover over "Our Models" dropdown
+        cy.get('.nav-submenu-trigger-label').contains('Our Models').trigger('mouseover');
     }
 
     clickOnHatchback(){
-        this.elements.hatchBackLink().click()
+         // Click on "Hatchbacks"
+        cy.contains('Hatchbacks').click();
+    
+        // Click on "Hatchbacks"
+        cy.contains('Hatchbacks').click();
+
     }
 
     clickAclassModel(){
-        //action to hover state
-        //this.elements.aClassLink.trigger('mouseover')
-        this.elements.aClassLink().click()
+        // Mouse over the "A Class" model
+        cy.get('.col2-10 a[href="/cars/a-class/hatchback"]').trigger('mouseover');
     }
 
     clickBuildYourCar()
     {
-        this.elements.buildYourCarLink().click()
+        // Click on "Build your car"
+        cy.contains('Build your car').click();
     }
+
+    selectDieselOption(){
+        // Select "Diesel" fuel type
+        cy.get('input[value="Diesel"]').click();
+    }
+
+ 
+    // Get the prices and save the highest and lowest values in a text file
+    getPrices(){
+        // Take a screenshot of the results
+        cy.get('.sales-price').then(($prices) => {
+            const prices = $prices.map((_, el) => Number(el.innerText.replace('£', ''))).toArray();
+            const highestPrice = Math.max(...prices);
+            const lowestPrice = Math.min(...prices);
+            const result = `Highest Price: £${highestPrice}\nLowest Price: £${lowestPrice}`;
+      
+            cy.writeFile('prices.txt', result);
+          });
+    }
+
 }
 
 
